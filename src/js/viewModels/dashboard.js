761e83b19a83c40c9309db71f9391535fabd149e
Alvin <alvinrobertlee@gmail.com>
2017-02-11 22:35:32 -0800

    Added an empty map

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -5,47 +5,48 @@
 /*
  * Your dashboard ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart'],
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'ojs/ojthematicmap'],
  function(oj, ko, $) {
 
     function DashboardViewModel() {
-      var self = this;
-      // Below are a subset of the ViewModel methods invoked by the ojModule binding
-      // Please reference the ojModule jsDoc for additionaly available methods.
-      var colorHandler = new oj.ColorAttributeGroupHandler();
-              this.lineWithAreaColor = colorHandler.getValue('lineWithAreaColor');
+        var self = this;
+        self.layers = [{layer: 'countries'}];
+        // Below are a subset of the ViewModel methods invoked by the ojModule binding
+        // Please reference the ojModule jsDoc for additionaly available methods.
+        var colorHandler = new oj.ColorAttributeGroupHandler();
+                this.lineWithAreaColor = colorHandler.getValue('lineWithAreaColor');
 
-              /* chart data */
-              var values = [5, 8, 2, 7, 0, 9, 2, 3, 4, 2];
+                /* chart data */
+                var values = [5, 8, 2, 7, 0, 9, 2, 3, 4, 2];
 
-              this.sparkValues = ko.observableArray(values);
+                this.sparkValues = ko.observableArray(values);
 
-              var rangeValues = [{low: 5, high: 9},
-                                 {low: 3, high: 7},
-                                 {low: 3, high: 8},
-                                 {low: 5, high: 10},
-                                 {low: 7, high: 14},
-                                 {low: 8, high: 13},
-                                 {low: 6, high: 11},
-                                 {low: 4, high: 9},
-                                 {low: 1, high: 5},
-                                 {low: 2, high: 8}];
+                var rangeValues = [{low: 5, high: 9},
+                                   {low: 3, high: 7},
+                                   {low: 3, high: 8},
+                                   {low: 5, high: 10},
+                                   {low: 7, high: 14},
+                                   {low: 8, high: 13},
+                                   {low: 6, high: 11},
+                                   {low: 4, high: 9},
+                                   {low: 1, high: 5},
+                                   {low: 2, high: 8}];
 
-              this.sparkRangeValues = ko.observableArray(rangeValues);
-      /**
-       * Optional ViewModel method invoked when this ViewModel is about to be
-       * used for the View transition.  The application can put data fetch logic
-       * here that can return a Promise which will delay the handleAttached function
-       * call below until the Promise is resolved.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
-       * the promise is resolved
-       */
-      self.handleActivated = function(info) {
-        // Implement if needed
-      };
+                this.sparkRangeValues = ko.observableArray(rangeValues);
+        /**
+         * Optional ViewModel method invoked when this ViewModel is about to be
+         * used for the View transition.  The application can put data fetch logic
+         * here that can return a Promise which will delay the handleAttached function
+         * call below until the Promise is resolved.
+         * @param {Object} info - An object with the following key-value pairs:
+         * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
+         * @param {Function} info.valueAccessor - The binding's value accessor.
+         * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
+         * the promise is resolved
+         */
+        self.handleActivated = function(info) {
+          // Implement if needed
+        };
 
       /**
        * Optional ViewModel method invoked after the View is inserted into the
